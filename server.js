0b3c6d2f6a0e40c14e718773b0be171fb3d14cfb
Hassan Saab <hassansaab@Hassans-MacBook-Pro.local>
2018-01-12 17:55:50 -0800

    added headers for CORS

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var pool = require('./pool.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", function(req, res) {
   pool.query(`SELECT * FROM signups`)
@@ -23,7 +28,6 @@ app.get("/add", function(req, res) {
       res.json(result.rows);
     })
     .catch((err) => {
-      console.log(req.query)
       res.status(400).json({error: err});
     })
 })
